e75e04f88097d2da56d2e418c9c40c9a6f7d3728
DhirendraBahadurBasnet <DhirendraBdrBasnet@gmail.com>
2025-06-29 19:55:30 +0545

    feat:added projects detail

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,22 +1,36 @@
-import Image from "next/image"
+import { Github } from "lucide-react";
+import Image from "next/image";
 
 const projects = [
   {
-    name: "Project 1",
-    description: "A brief description of Project 1",
+    name: "Vet Xray Web System",
+    description: `
+      Full-stack veterinary system with secure API and role-based access (Admin, Vet, Staff).
+      Built using React, Django REST, and PostgreSQL with JWT authentication.
+      Features include patient record handling, x-ray management, and CI/CD deployment via Render.
+    `,
     image: "/placeholder.svg?height=300&width=400",
+    githubLink: "https://github.com/zekarki/vet-xray-API",
   },
   {
-    name: "Project 2",
-    description: "A brief description of Project 2",
+    name: "Classroom Manager",
+    description: `
+      Desktop app built with C# Windows Forms for managing student records via binary random-access files.
+      Supports custom sort, search, persistent file I/O, and a user-friendly GUI with validation.
+    `,
     image: "/placeholder.svg?height=300&width=400",
+    githubLink: "https://github.com/zekarki/ClassroomPlan",
   },
   {
-    name: "Project 3",
-    description: "A brief description of Project 3",
+    name: "Personal Portfolio Website",
+    description: `
+      Responsive single-page portfolio built with Nextjs and TypeScript.
+      Deployed using Cloudflare Pages with smooth scrolling, dynamic routing, and modular design.
+    `,
     image: "/placeholder.svg?height=300&width=400",
+    githubLink: "https://github.com/zekarki/",
   },
-]
+];
 
 export default function Projects() {
   return (
@@ -37,7 +51,18 @@ export default function Projects() {
                 className="w-full h-48 object-cover"
               />
               <div className="p-6">
-                <h3 className="text-2xl font-semibold mb-2">{project.name}</h3>
+                <div className="flex items-center justify-between mb-2">
+                  <h3 className="text-2xl font-semibold ">{project.name}</h3>
+                  <a
+                    href={project.githubLink}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-foreground hover:text-slate-300 transition-colors duration-300"
+                    aria-label="GitHub Repository"
+                  >
+                    <Github size={20} />
+                  </a>
+                </div>
                 <p className="text-gray-300">{project.description}</p>
               </div>
             </div>
@@ -45,6 +70,5 @@ export default function Projects() {
         </div>
       </div>
     </section>
-  )
+  );
 }
-
